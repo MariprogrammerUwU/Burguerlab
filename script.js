d7576098ea09f5b1e3b9340482c5bca7586bd3ea
MariprogrammerUwU <programmerprojectmari@gmail.com>
2024-09-06 13:04:04 -0500

    Agrego un repositorio

---
+++ script.js
@@ -0,0 +1,14 @@
+// Make
+
+function mostrarMensaje() {
+    console.log('¡Alguien ha hecho clic en BURGER TOWN!');
+  }
+
+  function cambiarColor() {
+    document.getElementById('titulo').style.color = 'red';
+  }
+
+  function changeHeaderColor() {
+    // Cambia el color del h2 con id "burger-picture" a rojo
+    document.getElementById('burger-picture').style.color = 'red';
+  }
